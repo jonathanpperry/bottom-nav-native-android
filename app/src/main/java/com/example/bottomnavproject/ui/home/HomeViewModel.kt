179a8a00805f676137d4283ca79fa27a1756194f
Jonathan Perry <jpper@umich.edu>
2020-10-20 15:38:49 +0900

    Commit of starter tabs android application

---
+++ app/src/main/java/com/example/bottomnavproject/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.bottomnavproject.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is the home fragment"
+    }
+    val text: LiveData<String> = _text
+}
